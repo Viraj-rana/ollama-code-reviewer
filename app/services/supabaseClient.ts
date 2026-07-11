df8367551a678fe6eee9084c3ce3cb3db6714c58
viraj07 <hackteck404@gmail.com>
2026-07-11 22:17:18 +0300

    Update supabaseClient.ts

--- app/services/supabaseClient.ts
+++ app/services/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// For this MVP's, we will try to read them from env, or allow manual entry in settings.
+// for this MVP's, we will try to read them from env, or allow manual entry in settings.
 // Users must add VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY to their .env file.
 
 
